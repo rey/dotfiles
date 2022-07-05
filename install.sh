d41cf6222d372f76b534fd74df2e86951ce85e41
Rey Dhuny <rey@users.noreply.github.com>
2022-07-05 17:24:45 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/rey/.dot.git
+git clone https://github.com/rey/.vim.git
+git clone https://github.com/rey/.tmux.git
+
+
+
+
+
+bash .dot/install.sh
+bash .vim/install.sh
+bash .tmux/install.sh
